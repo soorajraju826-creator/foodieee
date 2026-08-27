e5ed266fce7dafc4deb3d18ac654a36fcd92138d
SOORAJ RAJU <soorajraju826@gmail.com>
2026-08-27 23:52:50 +0530

    fixx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,11 +3,12 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <nav>
-      <div>
-        <Link to="/">🍴 Foodie</Link>
-      </div>
+      <Link to="/" className="nav-logo">
+        <div className="nav-logo-icon">🍴</div>
+        <h2>Foodie</h2>
+      </Link>
 
-      <div>
+      <div className="nav-links">
         <Link to="/">Home</Link>
         <Link to="/cart">Cart</Link>
         <Link to="/orders">My Orders</Link>
